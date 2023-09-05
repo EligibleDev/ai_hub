8a31b3d6ef101f29bda26c123d5c9c559f6b3423
EligibleDev <eligibledev@gmail.com>
2023-09-06 01:10:08 +0600

    show all button added

--- js/app.js
+++ js/app.js
@@ -1,12 +1,21 @@
-const loadData = async (toolId) => {
+const loadData = async (isShowAll) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/ai/tools`)
     const data = await res.json();
     const tools = data.data.tools;
-    loadTools(tools);
+    loadTools(tools, isShowAll);
 }
 
-const loadTools = async (tools) => {
+const loadTools = async (tools, isShowAll) => {
+    const showAllButton = document.getElementById('show-all-button')
+    if(!isShowAll){
+        showAllButton.classList.remove('hidden'),
+        tools = tools.slice(0, 6);
+    }else{
+        showAllButton.classList.add('hidden');
+    }
+
     const cardGrid = document.getElementById('card-grid');
+    cardGrid.innerHTML = '';
 
     const card = document.createElement('div');
     card.classList.add('card');
@@ -37,9 +46,12 @@ const loadTools = async (tools) => {
                 <i class="fa-solid fa-arrow-right"></i>
             </button>
         </div>`;
-        
         cardGrid.appendChild(card);
-
     });
 }
+
+const displayAll = () => {
+    loadData(true);
+}
+
 loadData();
