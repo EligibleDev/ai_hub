1481cee1c37ece4739704b1e14eaaa6c75210dc5
EligibleDev <eligibledev@gmail.com>
2023-09-05 19:30:25 +0600

    ar val lagse na

--- js/app.js
+++ js/app.js
@@ -1,6 +1,45 @@
-const loadData = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/ai/tools')
+const loadData = async (toolId) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/ai/tools`)
     const data = await res.json();
-    console.log(data.data.tools);
+    const tools = data.data.tools;
+    loadTools(tools);
+}
+
+const loadTools = async (tools) => {
+    const cardGrid = document.getElementById('card-grid');
+
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    tools.forEach(tool => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        console.log(tool)
+
+        card.innerHTML = `
+        <img src="${tool.image}" alt="No Image Available">
+        <h3 class="title">Features</h3>
+        <ol>
+            ${tool.features.map(feature => `<li>${feature}</li>`).join("")}
+        </ol>
+        <hr>
+    
+        <div class="info">
+            <div>
+                <h3 class="title">${tool.name}</h3>
+                <div class="time">
+                    <i class="fa-solid fa-calendar-days"></i>
+                    <span class="date">${tool.published_in}</span>
+                </div>
+            </div>
+    
+            <button class="go-button">
+                <i class="fa-solid fa-arrow-right"></i>
+            </button>
+        </div>`;
+        
+        cardGrid.appendChild(card);
+
+    });
 }
 loadData();
