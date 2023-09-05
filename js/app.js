302295c5daef1e2691971865e0f4b35e84a75b12
EligibleDev <eligibledev@gmail.com>
2023-09-06 01:25:53 +0600

    done I guess

--- js/app.js
+++ js/app.js
@@ -14,6 +14,11 @@ const loadTools = async (tools, isShowAll) => {
         showAllButton.classList.add('hidden');
     }
 
+    const sortButton = document.getElementById('sort-button')
+    sortButton.addEventListener('click', function() {
+        sort(tools);
+    })
+
     const cardGrid = document.getElementById('card-grid');
     cardGrid.innerHTML = '';
 
@@ -52,6 +57,45 @@ const loadTools = async (tools, isShowAll) => {
 
 const displayAll = () => {
     loadData(true);
+}
+
+const sort = (sortedTools) => {
+    const cardGrid = document.getElementById('card-grid');
+    cardGrid.innerHTML = '';
+
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    const sorted = sortedTools.sort((x, y) => parseInt(x.published_in) - parseInt(y.published_in));
+
+    sorted.forEach(tool => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        console.log(tool)
+
+        card.innerHTML = `
+        <img src="${tool.image}" alt="No Image Available">
+        <h3 class="title">Features</h3>
+        <ol>
+            ${tool.features.map(feature => `<li>${feature}</li>`).join("")}
+        </ol>
+        <hr>
+    
+        <div class="info">
+            <div>
+                <h3 class="title">${tool.name}</h3>
+                <div class="time">
+                    <i class="fa-solid fa-calendar-days"></i>
+                    <span class="date">${tool.published_in}</span>
+                </div>
+            </div>
+    
+            <button class="go-button">
+                <i class="fa-solid fa-arrow-right"></i>
+            </button>
+        </div>`;
+        cardGrid.appendChild(card);
+    });
 }
 
 loadData();
